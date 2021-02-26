ca8c20e5b983bcc67525af231b256bc841e98ad8
Patrick Wehbe <pew02@mail.aub.edu>
2021-02-26 16:11:31 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+const port = process.env.PORT || 8081;
+
+app.get("/", (req, res) => {
+	res.send("Hello, World!");
+});
+
+app.listen(port, () => console.log(`app started at port: ${port}`));
